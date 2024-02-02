d4caa9ea9dcf70149f9c40d8593ed9fa21dc7986
Amish <amish.tufail2002@gmail.com>
2024-02-02 15:51:28 +0500

    - Added Parallax Effect of Image - Complete

--- ScrollParallexEffect/Home.swift
+++ ScrollParallexEffect/Home.swift
@@ -13,6 +13,34 @@ struct Home: View {
             LazyVStack(spacing: 15.0) {
                 dummySection(title: "Social Media")
                 dummySection(title: "Sales", isLong: true)
+                // Parallax Image
+                ParallaxImageView(maximumMovement: 150.0,useFullWidth: true) { size in
+                    Image(.imageOne)
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: size.width, height: size.height)
+                }
+                .frame(height: 300.0)
+                dummySection(title: "Social Media")
+                dummySection(title: "Sales", isLong: true)
+                ParallaxImageView(useFullWidth: true) { size in
+                    Image(.imageTwo)
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: size.width, height: size.height)
+                }
+                .frame(height: 300.0)
+                dummySection(title: "Social Media")
+                dummySection(title: "Sales", isLong: true)
+                ParallaxImageView(useFullWidth: true) { size in
+                    Image(.imageOne)
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: size.width, height: size.height)
+                }
+                .frame(height: 300.0)
+                dummySection(title: "Social Media")
+                dummySection(title: "Sales", isLong: true)
             }
             .padding(15.0)
         }
@@ -31,9 +59,36 @@ extension Home {
                 .kerning(1.2)
         }
         .frame(maxWidth: .infinity, alignment: .leading)
+        .clipped()
     }
 }
 
+struct ParallaxImageView<Content: View>: View {
+    var maximumMovement: CGFloat = 100
+    var useFullWidth: Bool = false
+    @ViewBuilder var content: (CGSize) -> Content
+    var body: some View {
+        GeometryReader {
+            let size = $0.size
+            // Movement animation properties
+            let minY = $0.frame(in: .scrollView(axis: .vertical)).minY
+            let scrollViewHeight = $0.bounds(of: .scrollView)?.size.height ?? 0
+            let maximumMovement = min(maximumMovement, (size.height * 0.35))
+            let strechtedSize: CGSize = .init(width: size.width, height: size.height + maximumMovement)
+            
+            let progress = minY / scrollViewHeight
+            let cappedProgress = max(min(progress,1.0), -1.0)
+            let movementOffset = cappedProgress * -maximumMovement
+            
+            content(size)
+                .offset(y: movementOffset)
+                .frame(width: strechtedSize.width, height: strechtedSize.height)
+                .frame(width: size.width, height: size.height)
+                .clipped()
+        }
+        .containerRelativeFrame(useFullWidth ? [.horizontal] : [])
+    }
+}
 
 #Preview {
     ContentView()
