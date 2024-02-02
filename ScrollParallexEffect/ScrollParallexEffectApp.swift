177dc0e5fda40a9d1667daebb6bb53c978d57d42
Amish <amish.tufail2002@gmail.com>
2024-02-02 14:01:50 +0500

    Initial Commit

---
+++ ScrollParallexEffect/ScrollParallexEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollParallexEffectApp.swift
+//  ScrollParallexEffect
+//
+//  Created by Amish Tufail on 02/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct ScrollParallexEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
